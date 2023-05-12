234da50b095b0ef2d689a9a07c10b2524ab868a7
hushendian <huxiaohuvip@163.com>
2023-05-12 14:42:37 +0800

    优化ui界面

--- app/src/main/java/com/jadlsoft/jadl_tyqbq/MainActivity.java
+++ app/src/main/java/com/jadlsoft/jadl_tyqbq/MainActivity.java
@@ -3,8 +3,12 @@ package com.jadlsoft.jadl_tyqbq;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         GsonUtils.INSTANCE.init();
+        setStatusBar();
         findViewById(R.id.jump).setOnClickListener(v -> {
             try {
                 Intent intent = getPackageManager().getLaunchIntentForPackage("com.jadl_tyxqbq.Z4_A8_61");
@@ -23,5 +28,19 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("qbgl", "请安装相应的apk");
             }
         });
+    }
+
+
+    void setStatusBar() {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().getDecorView().setSystemUiVisibility(getWindow().getDecorView().getSystemUiVisibility() | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            // 没有这行的话 navigationBarColor 设置不成功
+            getWindow().setNavigationBarContrastEnforced(false);
+        }
+        getWindow().setNavigationBarColor(Color.TRANSPARENT);
     }
 }
--- qbApp/src/main/java/com/example/myapplication/MainActivity.java
+++ qbApp/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.myapplication;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initView();
         bindService();
         setOnclick();
+        setStatusBar();
     }
 
     private void initView() {
@@ -221,4 +225,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         finish();
     }
 
+
+    void setStatusBar() {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().getDecorView().setSystemUiVisibility(getWindow().getDecorView().getSystemUiVisibility() | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            // 没有这行的话 navigationBarColor 设置不成功
+            getWindow().setNavigationBarContrastEnforced(false);
+        }
+        getWindow().setNavigationBarColor(Color.TRANSPARENT);
+    }
 }
